88b40fb481ed8092cc4e2f195ef5459891b2e57d
paul-koo <pavelkoltun999@gmail.com>
2024-05-24 22:36:26 +0300

    feat: add styledComponents 'Wrapper'

--- home-work-week-1-card/src/App.tsx
+++ home-work-week-1-card/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Wrapper>
+      <div>adasd</div>
+      <button>asdasd</button>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  max-width: 1440px;
+  margin: auto;
+  display: flex;
+`
 
 export default App;
